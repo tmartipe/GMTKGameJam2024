b784e4620a38e11a68dbb8cb373b85cfac3a98bf
Tomas Martinez <tomasurielmp@gmail.com>
2024-08-18 22:28:52 -0300

    Agregado el menu principal y solucionado problema posicion al escalar

--- Scripts/personaje.gd
+++ Scripts/personaje.gd
@@ -2,18 +2,19 @@ extends CharacterBody3D
 
 @onready var sprite = $Sprite3D
 var currentShape: CompressedTexture2D
-var currentScale: float = 0.1
 var Q:bool
 var W:bool
 var E:bool
-var scale_step = 0.1
-var min_scale = 0.1
-var max_scale = 0.5
-	
+var scale_step = Vector3(0.1,0.1,0.1)
+var min_scale := 0.21
+var max_scale := 0.4
+
+func _ready():
+	scale = Vector3(0.3,0.3,0.3)
 
 func _process(_delta):
 	readInputs()
-	changeMaterial()
+	changeShape()
 	pass
 	
 func readInputs():
@@ -21,18 +22,14 @@ func readInputs():
 	W = Input.is_action_pressed("W")
 	E = Input.is_action_pressed("E")
 	
-	if(Input.is_action_just_pressed("ScaleUp") && currentScale < max_scale):
-		currentScale += scale_step
-		set_object_scale()
-		
-	if(Input.is_action_just_pressed("ScaleDown") && currentScale > min_scale):
-		currentScale -= scale_step
-		set_object_scale()
-		
-func set_object_scale():
-	sprite.scale = Vector3(currentScale, currentScale, currentScale)
-	
-func changeMaterial():
+	if(Input.is_action_just_pressed("ScaleUp") && scale.x < max_scale):
+		scale += scale_step
+		position = Vector3(0,0,-3)
+	if(Input.is_action_just_pressed("ScaleDown") && scale.x > min_scale):
+		scale -= scale_step
+		position = Vector3(0,0,-3)
+
+func changeShape():
 	
 	#if (Q and W and E):
 		#currentShape = ShapesConstants.QWE
